e4acd50fdd8f0c7c93b24d0a6e449bc0839c7ed7
Nâmmm <95410079+nguyenngoclam18t@users.noreply.github.com>
2022-11-16 07:23:42 +0700

    Add files via upload

--- Todo/main.js
+++ Todo/main.js
@@ -1,22 +1,58 @@
 /**
  * @type {HTMLElement}
  */
-
-function completeToDo() {
+function completeToDoList() {
   var listUi = document.querySelector(".todo-list");
   listUi = listUi.querySelectorAll("li");
-  var removeElement = listUi[listUi.length - 1];
-  var changeColor = listUi[listUi.length - 1].querySelector("button");
+  var elementchange = listUi[listUi.length - 1];
+  var changeColor = elementchange.querySelector("button");
   changeColor.onclick = (event) => {
     if (event.target.style.backgroundColor != "red") {
       event.target.style.backgroundColor = "red";
+      alert("bạn đã hoàn thành việc này");
     } else {
       event.target.style.backgroundColor = "green";
+      alert("bạn chưa hoàn thành việc này");
     }
   };
-  var removeLi = listUi[listUi.length - 1].querySelector(".remove");
+}
+function removeToDoList() {
+  var listUi = document.querySelector(".todo-list");
+  listUi = listUi.querySelectorAll("li");
+  var elementchange = listUi[listUi.length - 1];
+  var removeLi = elementchange.querySelector(".remove");
   removeLi.onclick = (event) => {
-    removeElement.remove();
+    elementchange.remove();
+  };
+}
+function editToDoList(todoList) {
+  var clickCounter = 0;
+  var listUi = document.querySelector(".todo-list");
+  listUi = listUi.querySelectorAll("li");
+  var elementchange = listUi[listUi.length - 1];
+  var editLi = elementchange.querySelector(".btn.btn-warning");
+  editLi.onclick = (event) => {
+    clickCounter++;
+    console.log(clickCounter);
+    if (clickCounter % 2 != 0) {
+      //lấy ra đoạn text để lên form
+      var textChange = elementchange.querySelector(".todo__title_mb-0");
+      //lấy form
+      var formElement = document.querySelector("form");
+      //thay đổi text ở form
+      formElement.querySelector(".form-control").value = textChange.textContent;
+      formElement.onsubmit = (event) => {
+        event.preventDefault();
+        //lấy data
+        var data1 = formElement.querySelector(".form-control").value;
+        //gán data
+        if (!data1) return;
+        elementchange.querySelector(".todo__title_mb-0").textContent = data1;
+        formElement.querySelector(".form-control").value = "";
+      };
+    } else {
+      addElementByForm(todoList);
+    }
   };
 }
 
@@ -25,9 +61,13 @@ function addElementByForm(todoList) {
   formElement.onsubmit = (event) => {
     event.preventDefault();
     var data1 = formElement.querySelector(".form-control").value;
+    if (!data1) return;
     addElementLi(data1);
     todoList.push({ id: Math.random(), name: `${data1}` });
-    completeToDo();
+    completeToDoList();
+    removeToDoList();
+    editToDoList(todoList);
+    formElement.querySelector(".form-control").value = "";
   };
 }
 function addElementLi(element) {
@@ -42,7 +82,9 @@ function renderTodoList(todoList) {
   // logic here
   for (var index in todoList) {
     addElementLi(todoList[index].name);
-    completeToDo();
+    completeToDoList();
+    removeToDoList();
+    editToDoList(todoList);
   }
   addElementByForm(todoList);
 }
